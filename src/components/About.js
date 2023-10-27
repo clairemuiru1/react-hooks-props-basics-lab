1b4c5d8ec3174b0ad17e472ada43a1a2e439dfa1
clairemuiru1 <muiruclaire@gmail.com>
2023-10-27 08:06:44 +0300

    "Commit changes"

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,13 @@
 import React from "react";
-import links from "./links";
+import Links from "./Links";
 
 function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{props.bio}</p>
+      {props.bio && <p>{props.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <p>{props.github}</p>
-      <p>{props.linkedin}</p>
+      <links github={props.github} linkedin={props.linkedin}/> 
     </div>
   );
 }
